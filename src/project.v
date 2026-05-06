bd96757515ea5c908ba4c9d8e7494705a28fbb4b
blonghi <isabella.longhi@berkeley.edu>
2026-05-06 19:00:50 +0000

    info.yaml updates

--- src/project.v
+++ src/project.v
@@ -8,10 +8,10 @@
 module tt_um_uart (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire [7:0] uio_in,   // X IOs: Input path
+    output wire [7:0] uio_out,  // X IOs: Output path
+    output wire [7:0] uio_oe,   // X IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // X always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -29,7 +29,7 @@ module tt_um_uart (
   wire [7:0] rx_data;
   wire rx_valid;
 
-
+  assign tx_data = ui_in;
 
   // List all unused inputs to prevent warnings
   //wire _unused = &{ena, clk, rst_n, 1'b0};
@@ -67,8 +67,8 @@ module tt_um_uart (
     .rx_valid(rx_valid)
   );
 
-  ui_in = tx_data;
-  uo_out = rx_data;
+
+  assign uo_out = rx_data;
 
 
 
